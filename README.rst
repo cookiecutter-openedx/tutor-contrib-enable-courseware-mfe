1858a7a66c27539c9c98198d97c8fe9c0192d343
lpm0073 <lpm0073@gmail.com>
2022-06-24 09:03:39 -0500

    first commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+enable_courseware_mfe plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/myusername/tutor-contrib-enable_courseware_mfe
+
+Usage
+-----
+
+::
+
+    tutor plugins enable enable_courseware_mfe
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ tutorenable_courseware_mfe/plugin.py
@@ -0,0 +1,89 @@
+from glob import glob
+import os
+import pkg_resources
+
+from tutor import hooks
+
+from .__about__ import __version__
+
+
+################# Configuration
+config = {
+    # Add here your new settings
+    "defaults": {
+        "VERSION": __version__,
+    },
+    # Add here settings that don't have a reasonable default for all users. For
+    # instance: passwords, secret keys, etc.
+    "unique": {
+        # "SECRET_KEY": "\{\{ 24|random_string \}\}",
+    },
+    # Danger zone! Add here values to override settings from Tutor core or other plugins.
+    "overrides": {
+        # "PLATFORM_NAME": "My platform",
+    },
+}
+
+################# Initialization tasks
+# To run the script from templates/enable_courseware_mfe/tasks/myservice/init, add:
+# hooks.Filters.COMMANDS_INIT.add_item((
+#     "myservice",
+#     ("enable_courseware_mfe", "tasks", "myservice", "init"),
+# ))
+
+################# Docker image management
+# To build an image with `tutor images build myimage`, add a Dockerfile to templates/enable_courseware_mfe/build/myimage and write:
+# hooks.Filters.IMAGES_BUILD.add_item((
+#     "myimage",
+#     ("plugins", "enable_courseware_mfe", "build", "myimage"),
+#     "docker.io/myimage:\{\{ ENABLE_COURSEWARE_MFE_VERSION \}\}",
+#     (),
+# )
+# To pull/push an image with `tutor images pull myimage` and `tutor images push myimage`, write:
+# hooks.Filters.IMAGES_PULL.add_item((
+#     "myimage",
+#     "docker.io/myimage:\{\{ ENABLE_COURSEWARE_MFE_VERSION \}\}",
+# )
+# hooks.Filters.IMAGES_PUSH.add_item((
+#     "myimage",
+#     "docker.io/myimage:\{\{ ENABLE_COURSEWARE_MFE_VERSION \}\}",
+# )
+
+
+################# You don't really have to bother about what's below this line,
+################# except maybe for educational purposes :)
+
+# Plugin templates
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    pkg_resources.resource_filename("tutorenable_courseware_mfe", "templates")
+)
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("enable_courseware_mfe/build", "plugins"),
+        ("enable_courseware_mfe/apps", "plugins"),
+    ],
+)
+# Load all patches from the "patches" folder
+for path in glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutorenable_courseware_mfe", "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+# Load all configuration entries
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        (f"ENABLE_COURSEWARE_MFE_{key}", value)
+        for key, value in config["defaults"].items()
+    ]
+)
+hooks.Filters.CONFIG_UNIQUE.add_items(
+    [
+        (f"ENABLE_COURSEWARE_MFE_{key}", value)
+        for key, value in config["unique"].items()
+    ]
+)
+hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
